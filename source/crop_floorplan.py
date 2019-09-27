5efaf21603571e19782c95f706cd912b5c6226bd
Branka <branka@plateiq.com>
2019-09-27 16:03:19 -0700

    Add docopt and fix indexing

--- source/crop_floorplan.py
+++ source/crop_floorplan.py
@@ -1,9 +1,22 @@
+"""
+
+Usage:
+	crop_floorplan.py <src> <dst> [--padding=<padding>] [--low=<low_t>] [--high=<high_t>] [--show]
+
+Options:
+	-h --help     Show this screen
+"""
+
 import cv2
 import numpy as np
+from docopt import docopt
 
 PADDING = 5
 
-def crop_floorplan(floorplan, padding=PADDING, low_t=0, high_t=255, show=False):
+# this function crops images. It has the assumption that only the pixels with low_t or smaller value or 
+# with high_t or higher value are important and determine the size of the image
+
+def crop_floorplan(floorplan, padding=PADDING, low_t=5, high_t=250, show=False):
 	if len(floorplan.shape) > 2:
 		floorplan = cv2.cvtColor(floorplan, cv2.COLOR_BGR2GRAY)
 
@@ -13,10 +26,12 @@ def crop_floorplan(floorplan, padding=PADDING, low_t=0, high_t=255, show=False):
 	min_col = floorplan.shape[0]
 	max_col = -1
 
+	print(floorplan.shape)
+
 	for row in range(rows):
-		tmp = floorplan[:][row]
-		tmpind1=np.where(tmp <= 0)
-		tmpind2=np.where(tmp >= 255)
+		tmp = floorplan[:, row]
+		tmpind1=np.where(tmp <= low_t)
+		tmpind2=np.where(tmp >= high_t)
 
 		if len(tmpind1[0])==0:
 			tmpind1=[cols+1,-1]
@@ -34,9 +49,9 @@ def crop_floorplan(floorplan, padding=PADDING, low_t=0, high_t=255, show=False):
 	max_row = -1
 
 	for col in range(cols):
-		tmp = floorplan[:][col]
-		tmpind1=np.where(tmp <= 0)
-		tmpind2=np.where(tmp >= 255)
+		tmp = floorplan[col, :]
+		tmpind1=np.where(tmp <= low_t)
+		tmpind2=np.where(tmp >= high_t)
 
 		if len(tmpind1[0])==0:
 			tmpind1=[rows+1,-1]
@@ -63,4 +78,27 @@ def crop_floorplan(floorplan, padding=PADDING, low_t=0, high_t=255, show=False):
 		cv2.waitKey(0)
 
 	return floorplan[top:bottom+1, left:right+1]
+
+
+if __name__ == "__main__":
+	arguments = docopt(__doc__)
+	floorplan = arguments['<src>']
+	dest = arguments['<dst>']
+
+	floorplan = cv2.imread(floorplan)
+	padding = 5
+	if arguments['--padding']:
+		padding = int(arguments['<padding>'])
+	low = 0
+	if arguments['--low']:
+		low = int(arguments['--low'])
+	high = 255
+	if arguments['--high']:
+		high = int(arguments['--high'])
+	show = False
+	if arguments['--show']:
+		show = True
+
+	cropped = crop_floorplan(floorplan, padding=padding, low_t=low, high_t=high, show=show)
+	cv2.imwrite(dest, cropped)
 
